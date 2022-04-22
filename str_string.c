5b046d2d7326fd3d947854d3e4cfbbd9c97581f7
TheSalient <abdulazeezlawal236@gmail.com>
2022-04-22 11:41:12 +0100

    my printf function

---
+++ str_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * str_str - print a string
+ * @argu: list of arguments
+ *
+ * Return: the string found
+ */
+char *str_str(va_list argu)
+{
+char *str = va_arg(argu, char*);
+
+if (str == NULL)
+{
+str = "(null)";
+}
+return (str);
+}
